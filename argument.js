0adc8b4544f254896a24b4d7edc3bf86ed493640
Nicholas Boyer <niccboyer@gmail.com>
2025-01-19 10:05:57 -0800

    Add argument script utilizing Minimist

--- argument.js
+++ argument.js
@@ -1,3 +1,19 @@
-process.argv.forEach((val, idx) => {
-    console.log(`${idx}:${val}`);
-});
+const minimist = require('minimist');
+
+const PRINT_ARGS = true;
+const USE_MINIMIST = true;
+
+if (PRINT_ARGS) {
+    process.argv.forEach((val, idx) => {
+        console.log(`${idx}:${val}`);
+    });
+}
+
+if (USE_MINIMIST) {
+    const args = minimist(process.argv.slice(2));
+    if (args.name) {
+        console.log(`Hello, ${args.name}!`);
+    } else {
+        console.log(`No name provided. Use --name to specify a name.`);
+    }
+}
